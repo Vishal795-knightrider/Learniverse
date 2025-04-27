909b2f9962f2ecdc461bab3b9967997fcfb99bd0
Vivekpatel1234a <patelvpv369@gmail.com>
2025-04-27 13:56:27 +0530

    Update reset-password.js

--- reset-password.js
+++ reset-password.js
@@ -1,23 +1,4 @@
-// Get email from URL
-const urlParams = new URLSearchParams(window.location.search);
-const email = urlParams.get('email')?.toLowerCase(); // <-- Always use lowercase
-
-const resetForm = document.getElementById('reset-form');
-const message = document.getElementById('message');
-
-resetForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const newPassword = document.getElementById('new-password').value;
-
-  const storedUser = JSON.parse(localStorage.getItem(`user-${email}`));
-  if (storedUser) {
-    storedUser.password = newPassword;
-    localStorage.setItem(`user-${email}`, JSON.stringify(storedUser));
-    message.textContent = '✅ Password reset successfully! You can now login.';
-    setTimeout(() => {
-      window.location.href = 'index.html'; // redirect back to login
-    }, 2000);
-  } else {
-    message.textContent = '❌ User not found.';
-  }
-});
+const templateParams = {
+  to_email: email,
+  reset_link: `https://sprightly-biscotti-09fabf.netlify.app//reset-password.html?email=${encodeURIComponent(email)}`
+};
